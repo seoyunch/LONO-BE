0bca5d0fe09300df10d02bb2b732f5952fd7c199
seoyunch <yen731013@naver.com>
2025-12-30 11:04:37 +0900

    Initial commit

---
+++ src/test/java/com/example/lono/LonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
